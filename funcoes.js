1fede84ea6535784f8b666cfbdf20d3c9fe36035
Gilmar Zezilia Alves <62348174+Gilmar99@users.noreply.github.com>
2020-07-19 18:18:48 -0300

    Atualização
    Atualizando cores do nav, deixando left autor com for menor que 480px

--- funcoes.js
+++ funcoes.js
@@ -1,16 +1,16 @@
-$(function () {
-    $('.botaoMenu').click(function (event) {
+$(function() {
+    $('.botaoMenu').click(function(event) {
         event.stopPropagation();
         if (!$(this).hasClass('menuAtivo')) {
             $(this).addClass('menuAtivo');
-            $('nav ul').animate({'left': '0px'}, 300);
+            $('nav ul').animate({ 'left': '0px' }, 220);
         } else {
             $(this).removeClass('menuAtivo');
-            $('nav ul').animate({'left': '-100%'}, 300);
+            $('nav ul').animate({ 'left': '-100%' }, 300);
         }
-         $('body').on('click', function () {
-$('.botaoMenu').removeClass('menuAtivo');
-        $('nav ul').animate({'left': '-100%'}, 300);
+        $('body').on('click', function() {
+            $('.botaoMenu').removeClass('menuAtivo');
+            $('nav ul').animate({ 'left': '-100%' }, 300);
         });
     });
 
